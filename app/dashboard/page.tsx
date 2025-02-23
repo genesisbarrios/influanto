e5f7e56bb1889d6124d31030754ff92707822ca3
Genesis Barrios <genesisbarriosdev@gmail.com>
2025-02-22 21:13:08 -0500

    link in bio fix

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -26,7 +26,7 @@ export default function Dashboard() {
   };
 
   return (
-    <div className="flex min-h-screen bg-base-200">
+    <div className="flex min-h-screen bg-base-200 text-xs md:text-md">
       {/* Sidebar menu */}
       <aside className="w-1/4 sm:w-1/3 p-4 bg-base-100 sm:w-1/3 sm:p-8">
         <ul className="menu bg-base-100 w-full p-2 rounded-box">
