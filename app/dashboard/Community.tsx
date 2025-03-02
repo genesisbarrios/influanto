1a460d630ced1415b02df47f4bcf2c324a2def01
Genesis Barrios <genesisbarriosdev@gmail.com>
2025-03-02 00:44:19 -0500

    style update

--- app/dashboard/Community.tsx
+++ app/dashboard/Community.tsx
@@ -6,30 +6,23 @@ import User from "@/models/User";
 import React, { useEffect, useState } from 'react';
 import apiClient from "@/libs/api";
 import { useSession, signOut } from "next-auth/react";
-import ButtonSupport from "@/components/ButtonSupport";
-import ButtonEdit from "@/components/ButtonEdit";
-import { faInstagram, faFacebook, faTelegram, faTiktok, faSoundcloud, faLinkedin, faApple, faAmazon, faEtsy, faYoutube, faPatreon, faGithub, faWebAwesome, faWebflow, faTwitter, faSpotify, faBandcamp, faDeezer, faYoutubeSquare, faSquareYoutube } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGlobe, faLocation } from "@fortawesome/free-solid-svg-icons";
-import { set } from "mongoose";
+import { faInstagram, faFacebook, faTwitter, faSpotify, faYoutube } from "@fortawesome/free-brands-svg-icons";
 const fallbackImageUrl = "https://images.pexels.com/photos/399772/pexels-photo-399772.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
 
-const Community =  () => {
+const Community = () => {
   const [users, setUsers] = useState<any>();
-
   const [isLoading, setIsLoading] = useState(false);
   const [alert, setAlertt] = useState("");
-  
+
   const getUsers = async () => {
     try {
       const { data } = await apiClient.get("/get-users");
-      console.log(data);
       const filteredUsers = data.filter((user: any) => user.name && (user.bio || user.image || user.instagram || user.twitter || user.facebook || user.spotify || user.youtube));
       setUsers(filteredUsers);
     } catch (e) {
-      //console.error(e?.message);
       setAlertt(e?.message);
-    } 
+    }
   }
 
   useEffect(() => {
@@ -37,70 +30,68 @@ const Community =  () => {
   }, []);
 
   useEffect(() => {
-   if(!users){
-    getUsers();
-   }else{
-    //console.log("user:");
-    //console.log(user);
-   }
+    if (!users) {
+      getUsers();
+    }
   }, [users]);
-   // Check if user data is not yet loaded
+
   if (!users) {
     return <div>Loading...</div>;
-  }else if (users){
+  } else if (users) {
     return (
-     
       <div className="p-4 bg-white shadow rounded-md text-black">
-         <div className="w-full flex justify-between items-center">
-            <h2 className="text-2xl font-bold mb-2">Community</h2>
-          </div>
-          <br></br>
+        <div className="w-full flex justify-between items-center">
+          <h2 className="text-2xl font-bold mb-2">Community</h2>
+        </div>
+        <br />
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {users.map((user:any) => (
-           <a key={user._id} href={`https://influanto.com/${user.username}`} target="_blank" rel="noopener noreferrer" className="block">
-           <div className="bg-gray-100 p-4 rounded-lg shadow-md text-center">
-             <img
-               src={user.image || fallbackImageUrl}
-               alt={user.name}
-               width={50}
-               height={50}
-               className="rounded-full mx-auto mb-2"
-             />
-             <h3 className="text-sm font-semibold">{user.name}</h3>
-             <p className="text-xs text-gray-600">{user.bio || "No bio available"}</p>
-             <div className="flex justify-center space-x-3 mt-2">
-               {user.instagram && (
-                 <a href={user.instagram} target="_blank" rel="noopener noreferrer">
-                   <FontAwesomeIcon icon={faInstagram} className="text-pink-500" />
-                 </a>
-               )}
-               {user.twitter && (
-                 <a href={user.twitter} target="_blank" rel="noopener noreferrer">
-                   <FontAwesomeIcon icon={faTwitter} className="text-blue-400" />
-                 </a>
-               )}
-               {user.facebook && (
-                 <a href={user.facebook} target="_blank" rel="noopener noreferrer">
-                   <FontAwesomeIcon icon={faFacebook} className="text-blue-600" />
-                 </a>
-               )}
-               {user.spotify && (
-                 <a href={user.spotify} target="_blank" rel="noopener noreferrer">
-                   <FontAwesomeIcon icon={faSpotify} className="text-green-500" />
-                 </a>
-               )}
-               {user.youtube && (
-                 <a href={user.youtube} target="_blank" rel="noopener noreferrer">
-                   <FontAwesomeIcon icon={faYoutube} className="text-red-500" />
-                 </a>
-               )}
-             </div>
-           </div>
-         </a>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {users.map((user: any) => (
+            <a key={user._id} href={`https://influanto.com/${user.username}`} target="_blank" rel="noopener noreferrer" className="block">
+              <div className="bg-gray-100 p-4 rounded-lg shadow-md">
+                <div className="flex items-center space-x-4 mb-2">
+                  <img
+                    src={user.image || fallbackImageUrl}
+                    alt={user.name}
+                    width={50}
+                    height={50}
+                    className="rounded-full mr-2"
+                  />
+                  <h3 className="text-sm text-blue-500 font-semibold">{user.name}</h3>
+                </div>
+                <p className="text-xs text-gray-500 mb-2">{user.bio || "No bio available"}</p>
+                <div className="flex justify-center space-x-3">
+                  {user.instagram && (
+                    <a href={user.instagram} target="_blank" rel="noopener noreferrer">
+                      <FontAwesomeIcon icon={faInstagram} className="text-pink-500" />
+                    </a>
+                  )}
+                  {user.twitter && (
+                    <a href={user.twitter} target="_blank" rel="noopener noreferrer">
+                      <FontAwesomeIcon icon={faTwitter} className="text-blue-400" />
+                    </a>
+                  )}
+                  {user.facebook && (
+                    <a href={user.facebook} target="_blank" rel="noopener noreferrer">
+                      <FontAwesomeIcon icon={faFacebook} className="text-blue-600" />
+                    </a>
+                  )}
+                  {user.spotify && (
+                    <a href={user.spotify} target="_blank" rel="noopener noreferrer">
+                      <FontAwesomeIcon icon={faSpotify} className="text-green-500" />
+                    </a>
+                  )}
+                  {user.youtube && (
+                    <a href={user.youtube} target="_blank" rel="noopener noreferrer">
+                      <FontAwesomeIcon icon={faYoutube} className="text-red-500" />
+                    </a>
+                  )}
+                </div>
+              </div>
+            </a>
           ))}
         </div>
-        </div>
+      </div>
     );
   }
 };
