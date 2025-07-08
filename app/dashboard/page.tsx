d9a0c03a72b3d3e2f88e2aabc332dc908ae8ead5
Genesis Barrios <genesisbarriosdev@gmail.com>
2025-07-08 04:17:00 -0400

    styles

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -30,7 +30,7 @@ export default function Dashboard() {
   return (
     <div className="flex min-h-screen bg-base-200">
       {/* Sidebar menu */}
-      <aside className="w-1/4 sm:w-1/3 p-4 bg-base-100 sm:w-1/3 sm:p-8 xs:pr-8">
+      <aside className="w-1/4 sm:w-1/3 p-0.5 sm:p-4 bg-base-100 sm:p-8">
         <ul className="menu bg-base-100 w-full p-2 rounded-box text-xs sm:text-sm md:text-base lg:text-lg">
           <li>
             <button className="block p-2 hover:bg-blue-100 w-full" onClick={() => setActiveComponent('profile')}>Profile</button>
