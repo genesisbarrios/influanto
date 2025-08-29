9c2ef449fb46526cb3248a46316200aad4d0c4ec
Genesis Barrios <genesisbarriosdev@gmail.com>
2025-08-29 02:41:57 -0400

    fix seo img instagram

--- libs/seo.tsx
+++ libs/seo.tsx
@@ -31,20 +31,19 @@ export const getSEOTags = ({
         ? "http://localhost:3000/"
         : `https://${config.domainName}/`
     ),
-
     openGraph: {
       title: openGraph?.title || config.appName,
       description: openGraph?.description || config.appDescription,
       url: openGraph?.url || `https://${config.domainName}/`,
       siteName: openGraph?.title || config.appName,
-      // If you add an opengraph-image.(jpg|jpeg|png|gif) image to the /app folder, you don't need the code below
-      // images: [
-      //   {
-      //     url: `https://${config.domainName}/share.png`,
-      //     width: 1200,
-      //     height: 660,
-      //   },
-      // ],
+      //If you add an opengraph-image.(jpg|jpeg|png|gif) image to the /app folder, you don't need the code below
+      images: [
+        {
+          url: `https://${config.domainName}/SQUARE_LOGO.png`,
+          width: 500,
+          height: 500,
+        },
+      ],
       locale: "en_US",
       type: "website",
     },
@@ -88,7 +87,7 @@ export const renderSchemaTags = () => {
           url: `https://${config.domainName}/`,
           author: {
             "@type": "Person",
-            name: "Genesis Barrios",
+            name: "Gen Barrios",
           },
           datePublished: "2024-08-01",
           applicationCategory: "MarketingApplication",
