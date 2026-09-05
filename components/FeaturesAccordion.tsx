e1972d32a53341c7a2c9fc2bfd0ad60f6adc2c70
Genesis Barrios <genesisbarriosdev@gmail.com>
2026-09-05 12:02:49 -0400

    verbiage

--- components/FeaturesAccordion.tsx
+++ components/FeaturesAccordion.tsx
@@ -255,7 +255,7 @@ const FeaturesAccordion = () => {
     >
       <div className="px-8">
         <h2 className="font-extrabold text-4xl lg:text-6xl tracking-tight mb-12 md:mb-24">
-          All you need to market your music
+          Everything you need for music marketing
           <span className="bg-neutral text-neutral-content px-2 md:px-4 ml-1 md:ml-1.5 leading-relaxed whitespace-nowrap">
            in one place
           </span>
